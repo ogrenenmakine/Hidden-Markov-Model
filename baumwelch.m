f2ecfc782476ac6a21f9ba1fbc08180cd331e3d7
Mehmet Can Yavuz <ogrenenmakine@gmail.com>
2018-05-21 22:14:42 +0300

    train and val separated

--- baumwelch.m
+++ baumwelch.m
@@ -1,23 +1,33 @@
-function [A,B,h] = baumwelch(data, A, B, p, n, m)
-    nSeq = length(data);
+function [Amax,Bmax,h,hval] = baumwelch(train, val, A, B, p, n, m)
+    nSeq = length(train);
     logp = log(p);
     A = log(A);
     % random initialization
     alpha = log(rand(m,m));
-    phi = log(rand(n,m)); h = -Inf;
+    phi = log(rand(n,m)); hval = -Inf; h = -Inf;
     % EM variables initialization
-    logm = -Inf;
+    logmval = -Inf;
+    logmmax = -Inf;
     logmold = 0;
-    iter = 0;
+    iter = 1;
     for k = 1:250
-        logmold = logm;
-        [logm, alpha, beta, delta, gamma] = obsv_prob(data, A, B, logp, n);
-        [logp, A, B] = maximization(data, delta, gamma, B, n, m);
+        logmold = logmval;
+        [logm, alpha, beta, delta, gamma] = obsv_prob(train, A, B, logp, n);
+        [logp, A, B] = maximization(train, delta, gamma, B, n, m);
+        [logmval,~,~,~,~] = obsv_prob(train, A, B, logp, n);
+        if logmval > logmmax
+            Amax = A;
+            Bmax = B;
+            iter
+            logmmax = logmval;
+        end
         iter = iter + 1;
+        hval = cat(1,hval,logmval);
         h = cat(1,h,logm);
-        if iter > 1 & (abs(logmold-logm) <= 1e-5)
+        if iter > 1 & (abs(logmold-logmval) <= 1e-5)
             break
             h = h(2:end);
+            hval = hval(2:end);
         end
     end
 end
--- run.m
+++ run.m
@@ -1,15 +1,19 @@
 clc;clear;
 m = 2;
 n = 25;
-for i = 1:10
+for i = 1:1
     createRandModel(n, m);
     [A, B, p] = readModel('model.txt',n,m);
     test = dlmread('testdata.txt');
-    obsv_prob(test, A, B, p, n)
-    viterbi(test, A, B, p, n);
     data = dlmread('data.txt');
-    [A, B, h] = baumwelch(data, A, B, p, n, m);
+    ll = randperm(length(data));
+    train = data(1:(length(data)*4/5),:);
+    val = data((length(data)*4/5)+1:length(data),:);
+    [Amax, Bmax, h, hval] = baumwelch(train, val, A, B, p, n, m);
     obsv_prob(test, A, B, p, n)
-    plot(exp(h))
+    obsv_prob(test, Amax, Bmax, p, n)
+    viterbi(test, A, B, p, n);
+    plot(h)
     hold on;
+    plot(hval)
 end
